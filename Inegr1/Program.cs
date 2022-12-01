710c600aa5a341f9e32487f7246323a98f660c63
Philip Trubitsyn <trph@bk.ru>
2022-12-01 21:14:35 +0300

    квадрат

---
+++ Inegr1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число 1_");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2_");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b)
+{
+    Console.Write("Да!");
+}
+
+else
+{
+  Console.Write("Нет!");
+}
+
+
